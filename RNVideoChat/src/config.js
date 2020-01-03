d4ddad648a93e520b26b2f26af046b3abbea67c9
Joseph <jose>
2020-01-02 21:58:40 -0600

    init

---
+++ RNVideoChat/src/config.js
@@ -0,0 +1,23 @@
+export default [
+	{
+		appId: 1689,
+		authKey: 'Z8cQBp2CKaRYe63',
+		authSecret: 'NY2JOqastcYZ5wZ',
+	},
+	{
+		debug: { mode: 1 },
+	}
+]
+
+export const users = [
+		{
+			"id": 72780,
+			"login": "videouser1",
+			"password": "videouser1"
+		},
+		{
+			"id": 72781,
+			"login": "videouser2",
+			"password": "videouser2"
+		}
+	]
